c0d3ddd4d7af8d63b13596131ce23de54a4fabe9
Valentin Ledrapier <valentin.ledrapier@gmail.com>
2016-12-07 16:00:40 +0100

    Add skills page

--- src/scripts/router.js
+++ src/scripts/router.js
@@ -20,30 +20,50 @@ export default class Router {
     this.$els = {
       body: $('body'),
       content: $('.js-content'),
+      contentMenu: $('.js-contentMenu'),
       contentWrapper: $('.js-contentWrapper'),
+      pageName: $('.js-pageName'),
+      pages: $('.js-contentPage'),
+      page: {
+        projects: $('.js-contentPage[data-page="projects"]'),
+        skills: $('.js-contentPage[data-page="skills"]'),
+      }
     };
   }
 
   navigateToHome() {
     if (this.page !== 'home') {
         this.$els.content
-          .addClass('content--hidden');
+          .addClass('hidden');
+        this.$els.contentMenu
+          .addClass('hidden');
       this.page = 'home';
     }
   }
 
   navigateToProject($targetProject) {
+    this.$els.pageName.text('projets');
+    this.$els.page.skills.hide();
+    this.$els.page.projects.show();
+    this.$els.contentMenu
+      .removeClass('hidden');
 
     this.$els.content
-      .removeClass('content--hidden');
+      .removeClass('hidden');
 
-      this.$els.contentWrapper.scrollTop($targetProject.position().top);
+    this.$els.contentWrapper.scrollTop($targetProject.position().top);
     this.page = 'project';
   }
 
   navigateToSkills() {
+    this.$els.pageName.text('compétences');
+    this.$els.page.projects.hide();
+    this.$els.page.skills.show();
+    this.$els.contentMenu
+      .addClass('hidden');
+
     this.$els.content
-      .removeClass('content--hidden');
+      .removeClass('hidden');
 
     this.page = 'skills';
   }
